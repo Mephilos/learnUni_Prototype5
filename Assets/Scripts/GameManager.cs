ce9b7c39c83745cdf3bcc8dc93ff571bcfe217c8
TAEHO SIM <taehosim@TAEHOui-MacBookAir.local>
2025-04-28 12:38:34 +0900

    add ui, restart, gameover

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,30 @@
 using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+
 
 
 public class GameManager : MonoBehaviour
 {
+    private int score;
+    
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
     public float spawnRate = 1.0f;
-    private bool gameOver = false;
+    public bool isGameOver;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
+        isGameOver = false;
         StartCoroutine(SpawnTargets());
+        score = 0;
     }
 
     // Update is called once per frame
@@ -21,16 +34,25 @@ public class GameManager : MonoBehaviour
     }
     IEnumerator SpawnTargets()
     {
-        while(true)
+        while(!isGameOver)
         {
             yield return new WaitForSeconds(spawnRate);
             int targetIndex = Random.Range(0, targets.Count);
             Instantiate(targets[targetIndex]);
-
-            if(gameOver)
-            {
-                break;
-            }
         }
+    }
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+    public void GameOver()
+    {
+        isGameOver = true;
+        restartButton.gameObject.SetActive(true);
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,16 +1,21 @@
 
 //using System.Numerics;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Target : MonoBehaviour
 {
+    public int targetValue = 0;
     private float maxForce = 16.0f;
     private float minForce = 12.0f;
     private float randomTorque = 6.0f;
     private float randomSpawnPosX = 4.0f;
     private float spawnPosY = 0.0f;
+    public ParticleSystem boomParticle;
     private Rigidbody targetRb;
+    private GameManager gameManager;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,6 +23,8 @@ public class Target : MonoBehaviour
         targetRb.AddForce(FireForce(),ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -42,9 +49,19 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        Instantiate(boomParticle, transform.position, transform.rotation);
+        if(!gameManager.isGameOver)
+        {
+            gameManager.UpdateScore(targetValue);
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if(!gameObject.CompareTag("Bad"))
+        {
+            gameManager.gameOverText.gameObject.SetActive(true);
+            gameManager.GameOver();
+        }
     }
 }
